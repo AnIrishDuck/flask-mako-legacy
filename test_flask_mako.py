b0909a83370770167e1c720359000b9d16f04e14
Frank Murphy <fpmurphy@mtu.edu>
2012-07-05 16:58:39 -0400

    Cleaned up tests.

--- test_flask_mako.py
+++ test_flask_mako.py
@@ -1,43 +1,17 @@
 import os, tempfile, unittest
+from contextlib import contextmanager
 
 import flask
 from flask import Flask
 from flask.ext.mako import MakoTemplates, TemplateError
 
-basic = """
-% for arg in arguments:
-    ${arg}
-% endfor
-"""
-
-error = """
-% for arg in arguments:
-    ${error}
-% endfor
-"""
+from mako.exceptions import CompileException
 
 class MakoTemplateTest(unittest.TestCase):
     """ Tests the `flask_mako` templating extension. """
 
-    def _make_app(self):
-        app = Flask(__name__)
-        app.config.update(
-            MAKO_TEMPLATE_DIR=self.template_dir,
-            MAKO_CACHE_DIR=os.path.join(self.root, "cache"),
-            MAKO_CACHE_SIZE=10
-        )
-        return app
-
     def setUp(self):
-        self.root = root = tempfile.mkdtemp()
-
-        self.template_dir = os.path.join(root, "templates")
-
-        self.app = self._make_app()
-        self.mako = MakoTemplates(self.app)
-
-        for name, template in (('basic', basic), ('error_template', error)):
-            self._add_template(name, template)
+        self.root = tempfile.mkdtemp()
 
     def _add_template(self, name, text, d="templates"):
         template_dir = os.path.join(self.root, d)
@@ -47,25 +21,47 @@ class MakoTemplateTest(unittest.TestCase):
         with open(os.path.join(template_dir, name), 'w') as f:
             f.write(text.encode('utf8'))
 
+    @contextmanager
+    def test_renderer(self, **kwargs):
+        app = Flask(__name__)
+        app.config.update(
+            MAKO_TEMPLATE_DIR=os.path.join(self.root, "templates"),
+            MAKO_CACHE_DIR=os.path.join(self.root, "cache"),
+            MAKO_CACHE_SIZE=10
+        )
+        app.config.update(kwargs)
+        with app.test_request_context():
+            yield app, MakoTemplates(app)
+
     def test_encoding(self):
-        """ Tests that the application properly handles Unicode. """
-        utf = "\xC2\xA2"
-        self._add_template("unicode", utf)
+        """ Tests that the Mako templates properly handle Unicode. """
+        utf = u'\xA2'
+        self._add_template("unicode", utf + u'${arg}')
 
-        with self.app.test_request_context():
-            self.assertEqual(utf, self.mako.render('unicode'))
+        with self.test_renderer() as (_, mako):
+            result = mako.render('unicode', arg=utf)
+            self.assertEqual(utf * 2, result.decode("utf8"))
 
-        with self.assertRaises(TemplateError):
-            app = self.app
-            app.config.update(MAKO_OUTPUT_ENCODING='ascii')
-            ascii_mako = MakoTemplates(app)
-            with self.app.test_request_context():
-                ascii_mako.render('unicode')
+        with self.assertRaises(CompileException):
+            with self.test_renderer(MAKO_INPUT_ENCODING="ascii",
+                                    MAKO_CACHE_DIR=None) as (_, mako):
+                mako.render("unicode", arg="test")
+
+        with self.assertRaises(TemplateError) as e:
+            with self.test_renderer(MAKO_OUTPUT_ENCODING="ascii",
+                                    MAKO_CACHE_DIR=None) as (_, mako):
+                mako.render('unicode', arg='test')
+        self.assertEqual(e.exception.tb.errorname, "UnicodeEncodeError")
 
     def test_basic_template(self):
         """ Tests that the application can render a template. """
-        with self.app.test_request_context():
-            result = self.mako.render('basic', arguments=['testing', '123'])
+        self._add_template("basic", """
+        % for arg in arguments:
+            ${arg}
+        % endfor
+        """)
+        with self.test_renderer() as (_, mako):
+            result = mako.render('basic', arguments=['testing', '123'])
             self.assertTrue('testing' in result)
             self.assertTrue('123' in result)
 
@@ -85,28 +81,31 @@ class MakoTemplateTest(unittest.TestCase):
         ${injected()}
         """)
 
-        @self.app.route('/test')
-        def test(): return "test"
+        with self.test_renderer() as (app, mako):
 
-        @self.app.context_processor
-        def inject(): return {"injected": lambda: "injected"}
+            @app.route('/test')
+            def test(): return "test"
 
-        with self.app.test_request_context():
-            result = self.mako.render("vars")
+            @app.context_processor
+            def inject(): return {"injected": lambda: "injected"}
+
+            result = mako.render("vars")
 
     def test_signals(self):
         """ Tests that template rendering fires the right signal. """
         from flask.signals import template_rendered
-        log = []
 
-        def record(*args, **extra):
-            log.append(args)
-        template_rendered.connect(record, self.app)
+        self._add_template("signal", "signal template")
+        with self.test_renderer() as (app, mako):
 
-        with self.app.test_request_context():
-            result = self.mako.render('basic', arguments=[])
+            log = []
+            def record(*args, **extra):
+                log.append(args)
+            template_rendered.connect(record, app)
 
-        self.assertEqual(len(log), 1)
+            result = mako.render('signal')
+
+            self.assertEqual(len(log), 1)
 
     if not flask.signals_available:
         test_signals = unittest.skip("To test signals, install the blinker "
@@ -114,28 +113,47 @@ class MakoTemplateTest(unittest.TestCase):
 
     def test_multiple_dirs(self):
         """ Tests template loading from multiple directories. """
-        self._add_template("alt", "${alt}", "alt_templates")
-        alt_d = os.path.join(self.root, "alt_templates")
+        alts = ["alt_a", "alt_b", "alt_c"]
+        for alt in alts:
+            self._add_template(alt, alt + "${alt}", alt)
+            self._add_template(alt, alt + "${alt}", alt)
 
-        td = self.app.config['MAKO_TEMPLATE_DIR']
-        self.app.config.update(MAKO_TEMPLATE_DIR=[alt_d, td])
-        self.mako = MakoTemplates(self.app)
+        alt_dirs = [os.path.join(self.root, d) for d in alts]
+        with self.test_renderer(MAKO_TEMPLATE_DIR=alt_dirs) as (_, mako):
+            for alt in alts:
+                result = mako.render(alt, alt='testing')
+                self.assertTrue(alt in result)
+                self.assertTrue('testing' in result)
 
-        with self.app.test_request_context():
-            result = self.mako.render('alt', alt='testing')
-            self.assertTrue('testing' in result)
+    def test_multiple_apps(self):
+        """ Tests that the Mako plugin works with multiple Flask apps. """
+        self._add_template("app", "test 1", "alt1")
+        self._add_template("app", "test 2", "alt2")
+        alt1_dir = os.path.join(self.root, "alt1")
+        alt2_dir = os.path.join(self.root, "alt2")
+
+        with self.test_renderer(MAKO_TEMPLATE_DIR=alt1_dir) as (_, mako1):
+            self.assertEqual(mako1.render('app'), 'test 1')
+            with self.test_renderer(MAKO_TEMPLATE_DIR=[alt2_dir, alt1_dir],
+                                    MAKO_CACHE_DIR=None) as (_, mako2):
+                self.assertEqual(mako2.render('app'), 'test 2')
 
     def test_error(self):
         """ Tests that template errors are properly handled. """
-        with self.app.test_request_context():
+        self._add_template("error_template", """
+        % for arg in arguments:
+            ${error}
+        % endfor
+        """)
+
+        with self.test_renderer() as (app, mako):
             with self.assertRaises(TemplateError) as error:
-                self.mako.render('error_template', arguments=['x'])
+                mako.render('error_template', arguments=['x'])
 
             e = error.exception
             self.assertTrue('error_template' in e.message)
             self.assertTrue('error_template' in e.text)
             self.assertTrue('line 3' in e.text)
-
 
 def suite():
     suite = unittest.TestSuite()
